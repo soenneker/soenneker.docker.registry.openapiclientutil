249834102ad8121e01d9e72ee0b8155290fe9e82
soenneker <jake@soenneker.com>
2026-09-03 22:02:07 -0500

    Add interface inheritdoc references

--- src/Soenneker.Docker.Registry.OpenApiClientUtil/DockerRegistryOpenApiClientUtil.cs
+++ src/Soenneker.Docker.Registry.OpenApiClientUtil/DockerRegistryOpenApiClientUtil.cs
@@ -13,6 +13,7 @@ using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Docker.Registry.OpenApiClientUtil;
 
+/// <inheritdoc cref="IDockerRegistryOpenApiClientUtil" />
 public sealed class DockerRegistryOpenApiClientUtil : IDockerRegistryOpenApiClientUtil
 {
     private readonly AsyncSingleton<DockerRegistryOpenApiClient> _client;
